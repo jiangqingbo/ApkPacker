dff4d6905dcf70f1d3f6f2baf0579e2d68b5ce86
jiangqingbo <jqbo84@163.com>
2015-08-24 12:10:00 +0800

    修改读取Excel列表字段

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class MyFrame(wx.Frame):
     """
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, -1, title,
-                          pos=(150, 150), size=(640, 480))
+                          pos=(150, 150), size=(640, 520))
 
         # Create the menubar
         menuBar = wx.MenuBar()
@@ -44,7 +44,7 @@ class MyFrame(wx.Frame):
         self.SetMenuBar(menuBar)
 
         self.CreateStatusBar()
-        self.SetStatusText("Powered by Sam, 2014/12", 0)
+        self.SetStatusText("Powered by bobo, 2015/08", 0)
         
 
         self.cfgPath = os.getcwd()+"\\config.json"
@@ -70,33 +70,35 @@ class MyFrame(wx.Frame):
         line1.Add(excel, 0, wx.ALL,10)
         self.excel = excel
         
-        apks = []
-        if cfg != None and cfg.get("apk", None) != None:
-            apks = cfg.get("apk")
+#         apks = []
+#         if cfg != None and cfg.get("apk", None) != None:
+#             apks = cfg.get("apk")
             
         line2 = wx.BoxSizer(wx.HORIZONTAL);
-        btnOpenApk = wx.Button(panel, -1, "APK(1)")
+        btnOpenApk = wx.Button(panel, -1, "APK")
         self.Bind(wx.EVT_BUTTON, self.OnSelectAPK1, btnOpenApk)
         line2.Add(btnOpenApk)
         apk = wx.StaticText(panel, -1, "No selected")
         apk.SetFont(wx.Font(12, wx.SWISS, wx.NORMAL, wx.BOLD))
         apk.SetSize(apk.GetBestSize())
-        if len(apks) > 0 and apks[0] != None and len(apks[0]) > 1 :
-            apk.SetLabelText(apks[0]) 
+#         if len(apks) > 0 and apks[0] != None :
+#             apk.SetLabelText(apks[0]) 
+        if cfg != None and cfg.get("apk", None) != None:
+            apk.SetLabelText(cfg.get("apk"))
         line2.Add(apk, 0, wx.ALL,10)
-        self.apk1 = apk
+        self.apk = apk
         
-        line8 = wx.BoxSizer(wx.HORIZONTAL);
-        btnOpenApk2 = wx.Button(panel, -1, "APK(2)")
-        self.Bind(wx.EVT_BUTTON, self.OnSelectAPK2, btnOpenApk2)
-        line8.Add(btnOpenApk2)
-        apk = wx.StaticText(panel, -1, "No selected")
-        apk.SetFont(wx.Font(12, wx.SWISS, wx.NORMAL, wx.BOLD))
-        apk.SetSize(apk.GetBestSize())
-        if len(apks) > 1 and apks[1] != None and len(apks[1]) > 1 :
-            apk.SetLabelText(apks[1]) 
-        line8.Add(apk, 0, wx.ALL,10)
-        self.apk2 = apk
+#         line8 = wx.BoxSizer(wx.HORIZONTAL);
+#         btnOpenApk2 = wx.Button(panel, -1, "APK(2)")
+#         self.Bind(wx.EVT_BUTTON, self.OnSelectAPK2, btnOpenApk2)
+#         line8.Add(btnOpenApk2)
+#         apk = wx.StaticText(panel, -1, "No selected")
+#         apk.SetFont(wx.Font(12, wx.SWISS, wx.NORMAL, wx.BOLD))
+#         apk.SetSize(apk.GetBestSize())
+#         if len(apks) > 1 and apks[1] != None and len(apks[1]) > 1 :
+#             apk.SetLabelText(apks[1]) 
+#         line8.Add(apk, 0, wx.ALL,10)
+#         self.apk2 = apk
         
         line3 = wx.BoxSizer(wx.HORIZONTAL);
         btnOpenRar = wx.Button(panel, -1, "WinRar")
@@ -166,12 +168,30 @@ class MyFrame(wx.Frame):
         t2 = wx.TextCtrl(panel, -1, "", size=(300, -1), style=wx.TE_PASSWORD)
         #self.Bind(wx.EVT_TEXT, self.EvtText, t2)
         self.password = t2
+        
+        l3 = wx.Button(panel, -1, "ApkName")
+        t3 = wx.TextCtrl(panel, -1, "", size=(300, -1))
+        #t3.SetHelpText()
+        t3.SetHint("Input build ApkName, such as qmbb-v1.0.10.apk")
+        t3.SetLabelText("qmbb-v1.0.10.apk")
+        wx.CallAfter(t3.SetInsertionPoint, 0)
+        self.apkName = t3
+        
+        l4 = wx.Button(panel, -1, "aliasName")
+        t4 = wx.TextCtrl(panel, -1, "", size=(300, -1))
+        #t4.SetHelpText()
+        t4.SetHint("Input build ApkName, such as c:\newtank\newtank\quanminbb.keystore")
+        t4.SetLabelText("c:\\newtank\\newtank\quanminbb.keystore")
+        wx.CallAfter(t4.SetInsertionPoint, 0)
+        self.aliasName = t4
 
-        space = 6
+        space = 12
 
         line6 = wx.FlexGridSizer(cols=3, hgap=space, vgap=space)
         line6.AddMany([ l1, t1, (0,0),
-                        l2, t2, (0,0)
+                        l2, t2, (0,0),
+                        l3, t3, (0,0),
+                        l4, t4, (0,0)
                         ])
 
         #self.Bind(wx.EVT_TEXT, self.EvtText, t1)
@@ -181,7 +201,7 @@ class MyFrame(wx.Frame):
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.AddSizer(line1)
         sizer.AddSizer(line2)
-        sizer.AddSizer(line8)
+#         sizer.AddSizer(line8)
         sizer.AddSizer(line3)
         sizer.AddSizer(line4)
         sizer.AddSizer(line7)
@@ -189,7 +209,7 @@ class MyFrame(wx.Frame):
         sizer.AddSizer(line6)
         btnBuildScript = wx.Button(panel, -1, "Build Dos Script")
         self.Bind(wx.EVT_BUTTON, self.OnBuildScript, btnBuildScript)
-        sizer.Add(btnBuildScript, 0, wx.ALL, 10)
+        sizer.Add(btnBuildScript, 0, wx.ALL, 11)
         panel.SetSizer(sizer)
         panel.Layout()
 
@@ -208,8 +228,8 @@ class MyFrame(wx.Frame):
     def OnResetApks(self, evt):
         """Event handler for the button click."""
         print "OnResetApks"
-        self.apk1.SetLabelText("No selected")
-        self.apk2.SetLabelText("No selected") 
+        self.apk.SetLabelText("No selected")
+#         self.apk2.SetLabelText("No selected") 
 
     def OnFunButton(self, evt):
         """Event handler for the button click."""
@@ -273,42 +293,42 @@ class MyFrame(wx.Frame):
             # This returns a Python list of files that were selected.
             paths = dlg.GetPaths()
 
-            self.apk1.SetLabelText(paths[0])
+            self.apk.SetLabelText(paths[0])
 
         # Destroy the dialog. Don't do this until you are done with it!
         # BAD things can happen otherwise!
         dlg.Destroy()
-    def OnSelectAPK2(self, evt):
-        """Event handler for the button click."""
-        print "OnSelectAPK"
-        wildcard = "Android application (*.apk)|*.apk|"     \
-           "All files (*.*)|*.*"
-        # Create the dialog. In this case the current directory is forced as the starting
-        # directory for the dialog, and no default file name is forced. This can easilly
-        # be changed in your program. This is an 'open' dialog, and allows multitple
-        # file selections as well.
-        #
-        # Finally, if the directory is changed in the process of getting files, this
-        # dialog is set up to change the current working directory to the path chosen.
-        dlg = wx.FileDialog(
-            self, message="Choose android application",
-            defaultDir=os.getcwd(), 
-            defaultFile="",
-            wildcard=wildcard,
-            style=wx.OPEN | wx.CHANGE_DIR
-            )
-
-        # Show the dialog and retrieve the user response. If it is the OK response, 
-        # process the data.
-        if dlg.ShowModal() == wx.ID_OK:
-            # This returns a Python list of files that were selected.
-            paths = dlg.GetPaths()
-
-            self.apk2.SetLabelText(paths[0])
-
-        # Destroy the dialog. Don't do this until you are done with it!
-        # BAD things can happen otherwise!
-        dlg.Destroy()
+#     def OnSelectAPK2(self, evt):
+#         """Event handler for the button click."""
+#         print "OnSelectAPK"
+#         wildcard = "Android application (*.apk)|*.apk|"     \
+#            "All files (*.*)|*.*"
+#         # Create the dialog. In this case the current directory is forced as the starting
+#         # directory for the dialog, and no default file name is forced. This can easilly
+#         # be changed in your program. This is an 'open' dialog, and allows multitple
+#         # file selections as well.
+#         #
+#         # Finally, if the directory is changed in the process of getting files, this
+#         # dialog is set up to change the current working directory to the path chosen.
+#         dlg = wx.FileDialog(
+#             self, message="Choose android application",
+#             defaultDir=os.getcwd(), 
+#             defaultFile="",
+#             wildcard=wildcard,
+#             style=wx.OPEN | wx.CHANGE_DIR
+#             )
+# 
+#         # Show the dialog and retrieve the user response. If it is the OK response, 
+#         # process the data.
+#         if dlg.ShowModal() == wx.ID_OK:
+#             # This returns a Python list of files that were selected.
+#             paths = dlg.GetPaths()
+# 
+#             self.apk2.SetLabelText(paths[0])
+# 
+#         # Destroy the dialog. Don't do this until you are done with it!
+#         # BAD things can happen otherwise!
+#         dlg.Destroy()
     def OnSelectJarSigner(self, evt):
         """Event handler for the button click."""
         print "OnSelectJarSigner"
@@ -441,7 +461,7 @@ class MyFrame(wx.Frame):
             dlg.ShowModal()
             dlg.Destroy()
             return
-#         if self.apk1.GetLabelText() == "No selected":
+#         if self.apk.GetLabelText() == "No selected":
 #             dlg = wx.MessageDialog(self, 'Please set source apk(1) first',
 #                                'A Message Box',
 #                                wx.OK | wx.ICON_INFORMATION
@@ -502,14 +522,14 @@ class MyFrame(wx.Frame):
         rar = self.rar.GetLabelText()
         signer = self.signer.GetLabelText();
         keystore = self.store.GetLabelText();
-        apk1 = self.apk1.GetLabelText()
-        if apk1 == "No selected":
-            apk1 = None
-        apk2 = self.apk2.GetLabelText()
-        if apk2 == "No selected":
-            apk2 = None
-        if apk1 == None and apk2 == None: 
-            dlg = wx.MessageDialog(self, 'Please set source apk(1) or apk(2) first',
+        apk = self.apk.GetLabelText()
+        if apk == "No selected":
+            apk = None
+#         apk2 = self.apk2.GetLabelText()
+#         if apk2 == "No selected":
+#             apk2 = None
+        if apk == None: 
+            dlg = wx.MessageDialog(self, 'Please set source apk first',
                                 'A Message Box',
                                 wx.OK | wx.ICON_INFORMATION
                                 #wx.YES_NO | wx.NO_DEFAULT | wx.CANCEL | wx.ICON_INFORMATION
@@ -517,9 +537,9 @@ class MyFrame(wx.Frame):
             dlg.ShowModal()
             dlg.Destroy()
             return            
-        apk = (apk1, apk2)
+#         apk = (apk)
         self.saveConfig(worksapce, excel, apk, rar, signer, keystore)
-        self.buildDosScript(worksapce, excel, apk, rar, signer, self.buildNumber.GetLineText(0), self.password.GetLineText(0), keystore)
+        self.buildDosScript(worksapce, excel, apk, rar, signer, self.buildNumber.GetLineText(0), self.password.GetLineText(0), keystore, self.apkName.GetLineText(0), self.aliasName.GetLineText(0))
         
         
     def tryExcel(self, name):
@@ -537,44 +557,54 @@ class MyFrame(wx.Frame):
         print paramKeys
                     
                 
-        if sh.nrows > 3 :
-            for i in range(3, sh.nrows):
-                chanel = sh.cell_value(rowx=i, colx=0)
-                name = sh.cell_value(rowx=i, colx=1)
-                name =  name.strip()
-                if sh.ncols > 7:
-                    targetApk = sh.cell_value(rowx=i, colx=6)
-                    targetApk = targetApk.strip()
-                    if len(targetApk) > 0 :
-                        targetApk = "\\"+targetApk
-                    print targetApk
-                    for j in range(7, sh.ncols):
-                        key = paramKeys[j-7]
-                        if len(key) > 0:
-                            param = sh.cell_value(rowx=i, colx=j)
-                            if isinstance(param, float):
-                                param = '%d'%param
-                            line = "%s=%s"%(key,param)
-                            print line + ">>.\\assets\\conf.db\r\n"
-                key = key.strip()
-                paramKeys.append(key)
+#         if sh.nrows > 3 :
+#             for i in range(3, sh.nrows):
+#                 chanel = sh.cell_value(rowx=i, colx=0)
+#                 name = sh.cell_value(rowx=i, colx=1)
+#                 name =  name.strip()
+#                 if sh.ncols > 7:
+#                     targetApk = sh.cell_value(rowx=i, colx=6)
+#                     targetApk = targetApk.strip()
+#                     if len(targetApk) > 0 :
+#                         targetApk = "\\"+targetApk
+#                     print targetApk
+#                     
+#                     for j in range(6, sh.ncols):
+#                         key = paramKeys[j-7]
+#                         if len(key) > 0:
+#                             param = sh.cell_value(rowx=i, colx=j)
+#                             if isinstance(param, float):
+#                                 param = '%d'%param
+#                             line = "%s=%s"%(key,param)
+#                             print line + ">>.\\assets\\conf.db\r\n"
+#                     
+# #                     for j in range(7, sh.ncols):
+# #                         key = paramKeys[j-7]
+# #                         if len(key) > 0:
+# #                             param = sh.cell_value(rowx=i, colx=j)
+# #                             if isinstance(param, float):
+# #                                 param = '%d'%param
+# #                             line = "%s=%s"%(key,param)
+# #                             print line + ">>.\\assets\\conf.db\r\n"
+#                 key = key.strip()
+#                 paramKeys.append(key)
                 
         book.release_resources() 
         return
-    def buildDosScript(self, workspace, excel, apk, rar, signer, code, key, store):
+    def buildDosScript(self, workspace, excel, apk, rar, signer, code, key, store, appName, aliasName):
         print 'buildDosScript'
-        signCmd = u'"%s" -sigalg SHA1withRSA -digestalg SHA1 -verbose -storepass %s -keypass %s -keystore "%s" -signedjar eStockPre.apk app.apk estock\r\n' % (signer, key, key, store)
+        signCmd = u'"%s" -sigalg SHA1withRSA -digestalg SHA1 -verbose -storepass %s -keypass %s -keystore "%s" -signedjar %s app.apk %s\r\n' % (signer, key, key, store, appName, aliasName)
         bat = codecs.open("\\".join([workspace, "build.bat"]), 'w', "gbk")
         s1 = "REM global settings\r\nmkdir .\\assets\r\nmkdir .\\out\r\ncopy \""
-        s2 = "\" .\\app1.apk\r\n"
-        if apk[0] != None:
-            bat.write(s1+apk[0]+s2)
-            bat.write(u'"%s" d ".\\app1.apk" META-INF\\*\r\n' % rar)
-        s3 = "copy \""
-        s4 = "\" .\\app2.apk\r\n"
-        if apk[1] != None :
-            bat.write(s3+apk[1]+s4)
-            bat.write(u'"%s" d ".\\app2.apk" META-INF\\*\r\n' % rar)
+        s2 = "\" .\\app.apk\r\n"
+        if apk != None:
+            bat.write(s1+apk+s2)
+            bat.write(u'"%s" d ".\\app.apk" META-INF\\*\r\n' % rar)
+#         s3 = "copy \""
+#         s4 = "\" .\\app2.apk\r\n"
+#         if apk[1] != None :
+#             bat.write(s3+apk[1]+s4)
+#             bat.write(u'"%s" d ".\\app2.apk" META-INF\\*\r\n' % rar)
         #write line by line in excel
         book = xlrd.open_workbook(excel)
         print "The number of worksheets is", book.nsheets
@@ -591,46 +621,47 @@ class MyFrame(wx.Frame):
                 
         if sh.nrows > 3 :
             for i in range(3, sh.nrows):
-                mode = sh.cell_value(rowx=i, colx=7)
-                try:
-                    if len(mode)>0:
-                        mode = mode.lower();
-                except Exception as e:
-                    mode = ""
-                sourceApk = None
-                if mode == 'a' and apk[0] != None:
-                    sourceApk = "app1.apk"
-                if mode == 'b' and apk[1] != None:
-                    sourceApk = "app2.apk"
+#                 mode = sh.cell_value(rowx=i, colx=7)
+#                 try:
+#                     if len(mode)>0:
+#                         mode = mode.lower();
+#                 except Exception as e:
+#                     mode = ""
+#                 sourceApk = None
+                if apk != None:
+                    sourceApk = "app.apk"
+#                 if mode == 'b' and apk[1] != None:
+#                     sourceApk = "app2.apk"
                 if sourceApk == None :
                     continue
                 chanel = sh.cell_value(rowx=i, colx=0)
+                enName = sh.cell_value(rowx=i, colx=6)
                 name = sh.cell_value(rowx=i, colx=1)
                 name =  name.strip()
                 path = u'.\\out\\%s\\%s(%d)' % (code, name,chanel)
                 rem = u"\r\n\r\nREM pack "+path+u"\r\n"
                 bat.write(rem)
                 #out put script
-                s = u"echo %d=%s>.\\assets\\conf.db\r\n" % (chanel,code)
+                s = u"echo %d=%s>.\\assets\\conf.db\r\n" % (chanel,enName)
                 bat.write(s)
                 targetApk = ""
-                if sh.ncols > 8:
-                    targetApk = sh.cell_value(rowx=i, colx=6)
-                    targetApk = targetApk.strip()
-                    if len(targetApk) > 0 :
-                        targetApk = "\\"+targetApk
-                    for j in range(8, sh.ncols):
-                        key = paramKeys[j-8]
-                        if len(key) > 0:
-                            param = sh.cell_value(rowx=i, colx=j)
-                            if isinstance(param, float):
-                                param = '%d'%param
-                            s = "echo %s=%s >> .\\assets\\conf.db\r\n" % (key,param)
-                            bat.write(s)
+#                 if sh.ncols > 8:
+#                     targetApk = sh.cell_value(rowx=i, colx=6)
+#                     targetApk = targetApk.strip()
+#                     if len(targetApk) > 0 :
+#                         targetApk = "\\"+targetApk
+#                     for j in range(8, sh.ncols):
+#                         key = paramKeys[j-8]
+#                         if len(key) > 0:
+#                             param = sh.cell_value(rowx=i, colx=j)
+#                             if isinstance(param, float):
+#                                 param = '%d'%param
+#                             s = "echo %s=%s >> .\\assets\\conf.db\r\n" % (key,param)
+#                             bat.write(s)
                 bat.write(u'"%s" A ".\\%s" .\\assets\\conf.db\r\n' % (rar, sourceApk));
                 bat.write(signCmd.replace("app.apk", sourceApk))
                 bat.write(u"mkdir \""+path+u"\"\r\n")
-                bat.write(u"copy eStockPre.apk \""+path+targetApk+u"\"\r\n")
+                bat.write(u"copy " +appName+ u" \""+path+targetApk+u"\"\r\n")
                 
         book.release_resources()    
         bat.close()
@@ -670,7 +701,7 @@ class MyFrame(wx.Frame):
 
 class MyApp(wx.App):
     def OnInit(self):
-        frame = MyFrame(None, "Apk Packer v0.92")
+        frame = MyFrame(None, "Apk Packer v1.0.0")
         self.SetTopWindow(frame)
 
         print "Print statements go to this stdout window by default."
